ff550d4a26a50d439ffed3b12c110108b1ff755f
harsha <penumalaharshavardhan@gmail.com>
2024-08-27 10:52:20 +0000

    commit messge

---
+++ src/main/java/com/example/nxttrendz2/service/CategoryJpaService.java
@@ -0,0 +1,83 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.nxttrendz2.service;
+
+import com.example.nxttrendz2.model.Category;
+import com.example.nxttrendz2.repository.CategoryJpaRepository;
+import com.example.nxttrendz2.repository.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryJpaService implements CategoryRepository {
+    @Autowired
+    private CategoryJpaRepository categoryJpaRepository;
+
+    @Override
+    public ArrayList<Category> getAllCategories() {
+        List<Category> categoryList = categoryJpaRepository.findAll();
+        ArrayList<Category> category = new ArrayList<>(categoryList);
+        return category;
+    }
+
+    @Override
+    public Category getCategoryById(int categoryId) {
+        try {
+            Category category = categoryJpaRepository.findById(categoryId).get();
+            return category;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return categoryJpaRepository.save(category);
+    }
+
+    @Override
+    public Category updateCategory(int categoryId, Category category) {
+        try {
+            Category newCategory = categoryJpaRepository.findById(categoryId).get();
+            if (category.getName() != null) {
+                newCategory.setName(category.getName());
+            }
+            if (category.getDescription() != null) {
+                newCategory.setDescription(category.getDescription());
+            }
+            categoryJpaRepository.save(newCategory);
+            return newCategory;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteCategory(int categoryId) {
+        try {
+            categoryJpaRepository.deleteById(categoryId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+
+    }
+}
